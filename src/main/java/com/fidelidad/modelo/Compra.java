90cfe6d8d26300d6a4757d25fc5ab4778eb6ebab
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2025-07-04 23:20:50 -0400

    crearCompra_valoresInicialesCorrectos pasa

---
+++ src/main/java/com/fidelidad/modelo/Compra.java
@@ -0,0 +1,19 @@
+package com.fidelidad.modelo;
+
+public class Compra {
+    public String getIdCompra() {
+        return "B001";
+    }
+
+    public String getIdCliente() {
+        return "C001";
+    }
+
+    public Double getMonto() {
+        return 100.0;
+    }
+
+    public String getFecha() {
+        return "2023-10-01";
+    }
+} 
