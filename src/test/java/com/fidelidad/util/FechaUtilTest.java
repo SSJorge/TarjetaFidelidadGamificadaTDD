570aa6376bee192d31f2c77376745bb10ba5a1b5
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2025-07-04 23:54:11 -0400

    test parsear_fechaValida_retornaLocalDateEsperado falla

---
+++ src/test/java/com/fidelidad/util/FechaUtilTest.java
@@ -0,0 +1,13 @@
+package com.fidelidad.util;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FechaUtilTest {
+
+    @Test
+    void parsear_fechaValida_retornaLocalDateEsperado() {
+        LocalDate fecha = FechaUtil.parsear("01-10-23");
+        assertEquals(LocalDate.of(2023, 10, 1), fecha);
+    }
