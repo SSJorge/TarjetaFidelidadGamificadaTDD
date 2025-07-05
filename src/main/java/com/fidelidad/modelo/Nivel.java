c0b540987b3d7af3baffd3793ac6158b923d9acf
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2025-07-04 22:13:34 -0400

    test nivel_getMultiplicador_devuelveValorEsperado PASA

--- src/main/java/com/fidelidad/modelo/Nivel.java
+++ src/main/java/com/fidelidad/modelo/Nivel.java
@@ -2,8 +2,30 @@ package com.fidelidad.modelo;
 
 
 public enum Nivel {
-    BRONCE,
-    PLATA,
-    ORO,
-    PLATINO
+    BRONCE {
+        @Override
+        public double getMultiplicador() {
+            return 1.0;
+        }
+    },
+    PLATA {
+        @Override
+        public double getMultiplicador() {
+            return 1.1;
+        }
+    },
+    ORO {
+        @Override
+        public double getMultiplicador() {
+            return 1.2;
+        }
+    },
+    PLATINO {
+        @Override
+        public double getMultiplicador() {
+            return 1.3;
+        }
+    };
+
+    public abstract double getMultiplicador();
 }
