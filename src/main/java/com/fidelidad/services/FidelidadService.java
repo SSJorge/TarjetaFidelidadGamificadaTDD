d5d0a323506aaed77df6ed4a1237a73e3dbdd44f
Jorge Moreno <jorge.morenot@sansano.usm.cl>
2025-07-06 20:44:05 -0400

    test calcularPuntosASumar_redondeaHaciaAbajoPorCada100 refactorizacion

--- src/main/java/com/fidelidad/services/FidelidadService.java
+++ src/main/java/com/fidelidad/services/FidelidadService.java
@@ -2,10 +2,6 @@ package com.fidelidad.services;
 
 public class FidelidadService {
     public int calcularPuntosASumar(double monto) {
-        if (monto < 100) return 0;
-        if (monto < 200) return 1;
-        if (monto < 500) return 4;
-        if (monto < 800) return 7;
-        return 10; // Valor arbitrario si pasa de los ejemplos del test
+        return (int) (monto / 100);
     }
 }
